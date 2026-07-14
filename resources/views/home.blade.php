9c2b4b37d7d40a67a35f3d1b004f1793d8b0a9c0
AZDEVOPS\jfuentes <AZDEVOPS\jfuentes>
2026-07-14 08:00:24 -0600

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h3>Home</h3>
-</body>
-</html>
+<x-layout>
+    {{-- <x-slot:title>
+        Welcome
+    </x-slot:title> --}}
+    <x-slot:username>Jhon Doe</x-slot:username>
+    <main>
+        <h3>Home</h3>
+        <p>
+            This is your new label 
+            
+        </p>
+    </main>
+</x-layout>
